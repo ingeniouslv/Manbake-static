bd5f22afe713856722c3432deb0d29e38900b789
ingeniouslv <herb@ingeniouslv.com>
2013-10-21 22:35:22 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+/* ========================================================================
+	Mobile Test
+  ========================================================================= */
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+var testMobile = isMobile.any();  
+$(function() {
+	var wHeight = $(window).height();
+	$('#intro, #beefcakes-intro, #butters-intro, #manbake-contact').css({ 'height': wHeight });
+	$('#intro .item').css({ 'height': wHeight+300 });
+	$('#beefcakes').css({ 'min-height': wHeight+100 });
+});
+
+$(document).ready(function() {
+	$('.carousel.fade').carousel({
+		pause: false
+	});
+	$('#beefcake-carousel').carousel({
+		interval: false,
+		pause: 'hover'
+	});
+	$('#beefcake-carousel .item a, #beefcake-carousel button').on('click', function(event) {
+	    event.preventDefault();
+	    $('html, body').animate({
+	        scrollTop: $('#beefcake-carousel').offset().top-140
+	    }, 800);
+	});
+});
+
+$(window).resize(function() {
+	var wHeight = $(window).height();
+	$('#intro, #beefcakes-intro, #butters-intro, #manbake-contact').css({ 'height': wHeight });
+	$('#intro .item').css({ 'height': wHeight+300 });
+	$('#beefcakes').css({ 'min-height': wHeight+100 });
+});
+
+$(window).scroll(function() {
+		vScroll = $(window).scrollTop()/3;
+		$('#intro .carousel-inner').css("-webkit-transform", "translateY(-" + vScroll + "px)");
+		$('#intro .carousel-inner').css("-moz-transform", "translateY(-" + vScroll + "px)");
+		console.log(vScroll);
+});
